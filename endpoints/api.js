9bfe1082966b8a41c5f9c4dec3c50af188afa424
Fareed Babalola <kbdgreat165@yahoo.com>
2021-07-29 08:20:01 +0100

    .

--- endpoints/api.js
+++ endpoints/api.js
@@ -26,7 +26,7 @@ let cards = [];
 // });
 
 const pool = sql.createPool({
-    host: 'localhost',
+    host: '23.94.16.6',
     user: 'anniekuk_db',
     password: 'Allfordb007$',
     database: 'anniekuk_anniekuku',
