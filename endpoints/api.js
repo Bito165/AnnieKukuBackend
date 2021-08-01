237df57771f4d42254cfdf275eb28817349ab5be
Fareed Babalola <kbdgreat165@yahoo.com>
2021-08-01 17:14:40 +0100

    Update api.js

--- endpoints/api.js
+++ endpoints/api.js
@@ -26,10 +26,7 @@ let cards = [];
 // });
 
 const pool = sql.createPool({
-    connectionLimit: 1000000,
-    connectTimeout  : 60 * 60 * 1000,
-    acquireTimeout  : 60 * 60 * 1000,
-    timeout         : 60 * 60 * 1000,
+    connectionLimit: 1000000, 
     host: '23.94.16.6',
     user: 'anniekuk_data',
     password: 'Allforanniekuku007$',
