5f671a76bb99d1c6687078f39484202a8dbf43ef
Fareed Babalola <kbdgreat165@yahoo.com>
2021-11-16 11:13:08 +0100

    Push All

--- server.js
+++ server.js
@@ -10,117 +10,94 @@
 
         const app = express();
 
-        // const pool = sql.createPool({
-        //     connectionLimit: 1000000,
-        //     host: '127.0.0.1',
-        //     user: 'root',
-        //     password: '',
-        //     database: 'anniekuku',
-        //     multipleStatements: true
-        // });
-
-        // const pool = sql.createPool({
-        //     connectionLimit: 1000000,
-        //     connectTimeout  : 60 * 60 * 1000,
-        //     acquireTimeout  : 60 * 60 * 1000,
-        //     timeout         : 60 * 60 * 1000,
-        //     host: '23.94.16.6',
-        //     user: 'anniekuk_data',
-        //     password: 'Allforanniekuku007$',
-        //     database: 'anniekuk_database',
-        //     port: 3360,
-        //     multipleStatements: true
-        // });
+        const pool = sql.createPool({
+            connectionLimit: 1000000,
+            host: '127.0.0.1',
+            user: 'anniekuk_data',
+            password: 'Allforanniekuku007$',
+            database: 'anniekuk_database',
+            multipleStatements: true
+        });
 
         // const pool = sql.createPool('mysql://anniekuk_data:Allforanniekuku007$@23.94.16.6:3360/anniekuk_database?sslrootcert=rds-combined-ca-bundle.pem&sslmode=require');
 
 
-        const pool = sql.createPool({
-            connectionLimit: 1000000,
-            host: 'r6ze0q02l4me77k3.chr7pe7iynqr.eu-west-1.rds.amazonaws.com',
-            port: '3306',
-            user: 'cq7bslzg89bzf3rl',
-            password: 'v1kjmgf770pbvt4s',
-            database: 'wn9v9fnyzffqn302',
-            multipleStatements: true
+        // const pool = sql.createPool({
+        //     connectionLimit: 1000000,
+        //     host: 'r6ze0q02l4me77k3.chr7pe7iynqr.eu-west-1.rds.amazonaws.com',
+        //     port: '3306',
+        //     user: 'cq7bslzg89bzf3rl',
+        //     password: 'v1kjmgf770pbvt4s',
+        //     database: 'wn9v9fnyzffqn302',
+        //     multipleStatements: true
+        // });
+
+        
+        pool.getConnection((err, connection) => {
+
+    if (err){
+        console.log({err})
+    }else{
+
+        connection.query('CREATE TABLE IF NOT EXISTS products (id INT AUTO_INCREMENT PRIMARY KEY, item_name VARCHAR(255), item_avatar VARCHAR(255),item_avatar2 VARCHAR(255), item_avatar3 VARCHAR(255), old_price INT, item_price INT, item_category VARCHAR(255), item_quantity VARCHAR(255), quantity_sold VARCHAR(255),  item_description VARCHAR(255), createdby VARCHAR(255), createddate DATE)', (err, result) => {
+            if (err) throw err;
+        });
+
+        connection.query('CREATE TABLE IF NOT EXISTS orders (id INT AUTO_INCREMENT PRIMARY KEY, order_id VARCHAR(255), delivery_fee INT, amount INT ,number_of_items INT ,client_name VARCHAR(255), date_due VARCHAR(255), status INT, delivery_address VARCHAR(255), client_phone_number VARCHAR(255), transaction_ref VARCHAR(255), createddate DATE)', function (err, result) {
+            if (err) throw err;
+        });
+
+        connection.query('CREATE TABLE IF NOT EXISTS orderdetails (id INT AUTO_INCREMENT PRIMARY KEY, order_id VARCHAR(255), item_ordered VARCHAR(255), item_quantity INT, item_price INT, createddate DATE)',  (err, result) => {
+            if (err) throw err;
         });
 
 
-        pool.getConnection((err, connection) => {
+        connection.query('CREATE TABLE IF NOT EXISTS customers (id INT AUTO_INCREMENT PRIMARY KEY, customer_name VARCHAR(255), customer_phone_number VARCHAR(255), customer_email VARCHAR(255), source VARCHAR(255), number_of_orders INT, createdby VARCHAR(255), createddate DATE)', function (err, result) {
+            if (err) throw err;
+        });
 
-            if (err){
-                console.log(process.env.DATABASE_URL);
-                console.log({err})
-            }else{
+        connection.query('CREATE TABLE IF NOT EXISTS messages (id INT AUTO_INCREMENT PRIMARY KEY, sender VARCHAR(255), subject VARCHAR(255), content VARCHAR(255), status VARCHAR(255) ,createdby VARCHAR(255), createddate DATE, order_id VARCHAR(255))', function (err, result) {
+            if (err) throw err;
+        });
 
-            setInterval(() => {
-                connection.query('SELECT 1', function (err, res) {
-                    if (err) {
-                        connection.release();
-                    }
-                });
-            }, 1000);
+        connection.query('CREATE TABLE IF NOT EXISTS revenue (id INT AUTO_INCREMENT PRIMARY KEY, amount VARCHAR(255), source VARCHAR(255), staff VARCHAR(255), week_day VARCHAR(255), month VARCHAR(255), year VARCHAR(255), description VARCHAR(255), createdby VARCHAR(255), createddate DATE)', function (err, result) {
+            if (err) throw err;
+        });
 
-            connection.query('CREATE TABLE IF NOT EXISTS products (id INT AUTO_INCREMENT PRIMARY KEY, item_name VARCHAR(255), item_avatar LONGTEXT,item_avatar2 LONGTEXT, item_avatar3 LONGTEXT, old_price INT, item_price INT, item_category VARCHAR(255), item_quantity VARCHAR(255), quantity_sold VARCHAR(255),  item_description VARCHAR(255), createdby VARCHAR(255), createddate DATE)', (err, result) => {
-                if (err) throw err;
-            });
+        connection.query('CREATE TABLE IF NOT EXISTS expenses (id INT AUTO_INCREMENT PRIMARY KEY, amount VARCHAR(255), source VARCHAR(255), week_day VARCHAR(255), month VARCHAR(255), year VARCHAR(255), description VARCHAR(255), createdby VARCHAR(255), createddate DATE)', function (err, result) {
+            if (err) throw err;
+        });
 
+        connection.query('CREATE TABLE IF NOT EXISTS users (id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(255), full_name VARCHAR(255), title VARCHAR(255), password VARCHAR(255), last_login VARCHAR(255), user_type VARCHAR(255), createdby VARCHAR(255), createddate DATE)', function (err, result) {
+            if (err) throw err;
 
-            connection.query('CREATE TABLE IF NOT EXISTS orders (id INT AUTO_INCREMENT PRIMARY KEY, order_id VARCHAR(255), delivery_fee INT, amount INT ,number_of_items INT ,client_name VARCHAR(255), date_due VARCHAR(255), status INT, delivery_address VARCHAR(255), client_phone_number VARCHAR(255), transaction_ref VARCHAR(255), createddate DATE)', function (err, result) {
-                if (err) throw err;
-            });
-
-            connection.query('CREATE TABLE IF NOT EXISTS orderdetails (id INT AUTO_INCREMENT PRIMARY KEY, order_id VARCHAR(255), item_ordered VARCHAR(255), item_quantity INT, item_price INT, createddate DATE)',  (err, result) => {
-                if (err) throw err;
-            });
+        });
 
 
-            connection.query('CREATE TABLE IF NOT EXISTS customers (id INT AUTO_INCREMENT PRIMARY KEY, customer_name VARCHAR(255), customer_phone_number VARCHAR(255), customer_email VARCHAR(255), source VARCHAR(255), number_of_orders INT, createdby VARCHAR(255), createddate DATE)', function (err, result) {
-                if (err) throw err;
-            });
+        connection.query('CREATE TABLE IF NOT EXISTS productcategories (id INT AUTO_INCREMENT PRIMARY KEY, category_name VARCHAR(255), createdby VARCHAR(255), createddate DATE)', function (err, result) {
+            if (err) throw err;
 
-            connection.query('CREATE TABLE IF NOT EXISTS messages (id INT AUTO_INCREMENT PRIMARY KEY, sender VARCHAR(255), subject VARCHAR(255), content VARCHAR(255), status VARCHAR(255) ,createdby VARCHAR(255), createddate DATE, order_id VARCHAR(255))', function (err, result) {
-                if (err) throw err;
-            });
+        });
 
-            connection.query('CREATE TABLE IF NOT EXISTS revenue (id INT AUTO_INCREMENT PRIMARY KEY, amount VARCHAR(255), source VARCHAR(255), staff VARCHAR(255), week_day VARCHAR(255), month VARCHAR(255), year VARCHAR(255), description VARCHAR(255), createdby VARCHAR(255), createddate DATE)', function (err, result) {
-                if (err) throw err;
-            });
+        connection.query('CREATE TABLE IF NOT EXISTS revenuesources (id INT AUTO_INCREMENT PRIMARY KEY, source VARCHAR(255), createdby VARCHAR(255), createddate DATE)', function (err, result) {
+            if (err) throw err;
+        });
 
-            connection.query('CREATE TABLE IF NOT EXISTS expenses (id INT AUTO_INCREMENT PRIMARY KEY, amount VARCHAR(255), source VARCHAR(255), week_day VARCHAR(255), month VARCHAR(255), year VARCHAR(255), description VARCHAR(255), createdby VARCHAR(255), createddate DATE)', function (err, result) {
-                if (err) throw err;
-            });
+        connection.query('CREATE TABLE IF NOT EXISTS expensesources (id INT AUTO_INCREMENT PRIMARY KEY, source VARCHAR(255), createdby VARCHAR(255), createddate DATE)', function (err, result) {
+            if (err) throw err;
+        });
 
-            connection.query('CREATE TABLE IF NOT EXISTS users (id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(255), full_name VARCHAR(255), title VARCHAR(255), password VARCHAR(255), last_login VARCHAR(255), user_type VARCHAR(255), createdby VARCHAR(255), createddate DATE)', function (err, result) {
-                if (err) throw err;
+        connection.query('CREATE TABLE IF NOT EXISTS carousel (id INT AUTO_INCREMENT PRIMARY KEY, image VARCHAR(255), main_caption VARCHAR(255), min_caption VARCHAR(255), createdby VARCHAR(255), createddate DATE)', function (err, result) {
+            if (err) throw err;
+        });
 
-            });
+        connection.query('CREATE TABLE IF NOT EXISTS giftcards (id INT AUTO_INCREMENT PRIMARY KEY, code VARCHAR(255), amount_left VARCHAR(255), number_of_times_used VARCHAR(255), createddate DATE)', function (err, result) {
+            if (err) throw err;
+        });
 
+    }
 
-            connection.query('CREATE TABLE IF NOT EXISTS productcategories (id INT AUTO_INCREMENT PRIMARY KEY, category_name VARCHAR(255), createdby VARCHAR(255), createddate DATE)', function (err, result) {
-                if (err) throw err;
-
-            });
-
-            connection.query('CREATE TABLE IF NOT EXISTS revenuesources (id INT AUTO_INCREMENT PRIMARY KEY, source VARCHAR(255), createdby VARCHAR(255), createddate DATE)', function (err, result) {
-                if (err) throw err;
-            });
-
-            connection.query('CREATE TABLE IF NOT EXISTS expensesources (id INT AUTO_INCREMENT PRIMARY KEY, source VARCHAR(255), createdby VARCHAR(255), createddate DATE)', function (err, result) {
-                if (err) throw err;
-            });
-
-            connection.query('CREATE TABLE IF NOT EXISTS carousel (id INT AUTO_INCREMENT PRIMARY KEY, image LONGTEXT, main_caption VARCHAR(255), min_caption VARCHAR(255), createdby VARCHAR(255), createddate DATE)', function (err, result) {
-                if (err) throw err;
-            });
-
-            connection.query('CREATE TABLE IF NOT EXISTS giftcards (id INT AUTO_INCREMENT PRIMARY KEY, code VARCHAR(255), amount_left VARCHAR(255), number_of_times_used VARCHAR(255), createddate DATE)', function (err, result) {
-                if (err) throw err;
-            });
-
-        }
-
-        })
+})
 
 
         // Parsers for POST data
@@ -164,13 +141,13 @@
 
         // Catch all other routes and return the index file
         // app.get('*', (req, res) => {
-        //   res.sendFile(path.join(__dirname, 'dist/index.html'));
+        //   res.sendFile('index.html');
         // });
 
         /**
          * Get port from environment and store in Express.
          */
-        const port = '1011';
+        const port = process.env.PORT || '20111';
         app.set('port', port);
 
         /**
